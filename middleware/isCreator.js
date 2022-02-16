724c8fccf77c7c04e8c554befa1710e4a8cb470a
mgispert <mcg-511@hotmail.com>
2022-02-16 18:38:13 +0100

    delete console.logs

--- middleware/isCreator.js
+++ middleware/isCreator.js
@@ -5,10 +5,6 @@ module.exports = (req, res, next) => {
   Spot.findById(req.params.spotId)
     .populate("creator")
     .then((spotFromDB) => {
-      console.log("this is the req.params._id", req.params.spotId);
-      console.log("this is the spot from DB", spotFromDB);
-      console.log("this is the creator ", spotFromDB.creator._id);
-      console.log("this is the id of the current user", req.session.user._id);
       if (spotFromDB.creator.email !== req.session.user.email) {
         res.render("error");
         return;
