2b2cbf099eef6ee9a8ec39e909ccd61c4ed237da
mgispert <mcg-511@hotmail.com>
2022-02-16 18:37:17 +0100

    improve functionality to edit and delete spots / middleware function

--- middleware/isCreator.js
+++ middleware/isCreator.js
@@ -1,9 +1,19 @@
+const Spot = require("../models/Spot.model");
+const User = require("../models/User.model");
+
 module.exports = (req, res, next) => {
-  // checks if the user is logged in when trying to access a specific page
-  if (req.body.creator !== req.session.user._id) {
-    res.render("error");
-    return;
-  }
-  req.user = req.session.user;
-  next();
+  Spot.findById(req.params.spotId)
+    .populate("creator")
+    .then((spotFromDB) => {
+      console.log("this is the req.params._id", req.params.spotId);
+      console.log("this is the spot from DB", spotFromDB);
+      console.log("this is the creator ", spotFromDB.creator._id);
+      console.log("this is the id of the current user", req.session.user._id);
+      if (spotFromDB.creator.email !== req.session.user.email) {
+        res.render("error");
+        return;
+      }
+      req.user = req.session.user;
+      next();
+    });
 };
